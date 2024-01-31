efa1019effce921148018888f9a9204ba96cbb39
Hallvard-Benan <hallvard.benan@gmail.com>
2024-01-31 11:24:27 +0100

    init firebase

--- src/js/backend/setup.js
+++ src/js/backend/setup.js
@@ -1,3 +1,20 @@
 // do set up and initialisation
 // use import.meta.env.VARIABLE_NAME to access env variables (Vite-specific)
 // someValue = import.meta.env.VITE_SOME_VALUE
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdi7D8oGIdAxc97PuTClqsb6r3tb0owYA",
+  authDomain: "testproject-hb.firebaseapp.com",
+  projectId: "testproject-hb",
+  storageBucket: "testproject-hb.appspot.com",
+  messagingSenderId: "413143269341",
+  appId: "1:413143269341:web:51b1eb1b7efb5cb4ee8034",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
